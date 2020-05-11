b9bf823ff0ae1bb49939098a531240eaafa81d6c
chrismclarke <chris.m.clarke@live.co.uk>
2020-05-11 11:16:53 -0700

    stories service fix, styling, bindings

--- src/app/songs/songs.page.ts
+++ src/app/songs/songs.page.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Song } from './song.model';
-import { SongService } from './song.service';
+import { Component, OnInit } from "@angular/core";
+import { Song } from "./song.model";
+import { SongService } from "./song.service";
 
 @Component({
-  selector: 'plh-songs',
-  templateUrl: './songs.page.html',
-  styleUrls: ['./songs.page.scss'],
+  selector: "plh-songs",
+  templateUrl: "./songs.page.html",
+  styleUrls: ["./songs.page.scss"],
 })
 export class SongsPage implements OnInit {
-
   songList: Song[];
 
-  constructor(private songService: SongService) { 
+  constructor(songService: SongService) {
+    console.log("hello song service");
     songService.getSongList().subscribe((songs) => {
       this.songList = songs;
     });
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
