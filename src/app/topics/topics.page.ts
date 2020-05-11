54e52e7f4299943fa3feedb8f9589277d932681e
chrismclarke <chris.m.clarke@live.co.uk>
2020-05-11 15:46:12 -0700

    add tips slider

--- src/app/topics/topics.page.ts
+++ src/app/topics/topics.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IonSlides } from "@ionic/angular";
 
 @Component({
   selector: "app-topics",
@@ -6,33 +7,47 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./topics.scss"],
 })
 export class TopicsPage implements OnInit {
-  topics = [
-    {
-      title: "One-on-One Time",
-      number: 1,
-    },
-    {
-      title: "Keeping It Positive",
-      number: 2,
-    },
-    {
-      title: "Structure Up",
-      number: 3,
-    },
-    {
-      title: "Bad Behavior",
-      number: 4,
-    },
-    {
-      title: "Keep Calm and Manage Stress",
-      number: 5,
-    },
-    {
-      title: "Talking about Demo-1-19",
-      number: 6,
-    },
-  ];
+  topics = TOPICS;
+  // note - options passed to swiper api: https://swiperjs.com/api/
+  slideOptions: IonSlides["options"] = {
+    autoHeight: true,
+  };
+  @ViewChild("slides", { static: true }) slides: IonSlides;
 
   constructor() {}
   ngOnInit() {}
+
+  slideTo(index: number) {
+    console.log("sliding to index", index, this.slides);
+    this.slides.slideTo(index);
+  }
+
+  slideDidChange() {}
 }
+
+const TOPICS = [
+  {
+    title: "One-on-One Time",
+    number: 1,
+  },
+  {
+    title: "Keeping It Positive",
+    number: 2,
+  },
+  {
+    title: "Structure Up",
+    number: 3,
+  },
+  {
+    title: "Bad Behavior",
+    number: 4,
+  },
+  {
+    title: "Keep Calm and Manage Stress",
+    number: 5,
+  },
+  {
+    title: "Talking about Demo-1-19",
+    number: 6,
+  },
+];
