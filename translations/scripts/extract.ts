9e9d5108028858ddd911a5adca52c979e8314316
chrismclarke <chris.m.clarke@live.co.uk>
2020-06-10 23:22:58 -0700

    fix extraction scripts

--- translations/scripts/extract.ts
+++ translations/scripts/extract.ts
@@ -1,20 +1,20 @@
-import { spawnSync } from 'child_process';
-import * as fs from 'fs-extra';
+import { spawnSync } from "child_process";
+import * as path from "path";
+import * as fs from "fs-extra";
 
 function main() {
   // extract strings via ngx-extract
-  const outFile = './src/assets/i18n/app-strings.json';
+  // call from node_modules instead of regular npm script to allow custom methods after execution
+  const binPath = path.resolve(
+    process.cwd(),
+    "node_modules/.bin/ngx-translate-extract"
+  );
+  const outFile = "./src/assets/i18n/app-strings.json";
   spawnSync(
-    `npx ngx-translate-extract --input ./src --output ${outFile} --key-as-default-value --replace --format json`,
-    { shell: true, stdio: 'inherit' }
+    `${binPath} --input ./src --output ${outFile} --key-as-default-value --replace --format namespaced-json`,
+    { shell: true, stdio: "inherit" }
   );
-  //  on windows --key-as-default-value doesn't work correctly so manually rewrite json to create en strings
-  const json = fs.readJSONSync(outFile);
-  const processedJson = {};
-  Object.entries(json).forEach(
-    ([key, value]) => (processedJson[key] = value ? value : key)
-  );
-  fs.writeJSONSync(outFile, processedJson);
+  // create additional copy as default en strings
+  fs.copyFileSync(outFile, outFile.replace("app-strings", "app-strings-en"));
 }
 main();
-// ngx-translate-extract --input ./src --output ./src/assets/i81n/app-strings.json --key-as-default-value --replace --format json
