8b1ca977349949194f7d8b8d6c6ae807f5339804
chrismclarke <chris.m.clarke@live.co.uk>
2020-06-23 21:09:23 -0700

    code tidying

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,11 +4,8 @@ import { InfoSitesDisclaimer } from "./info-sites-disclaimer";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { PartnersFooterComponent } from "./partners-footer";
-import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { LanguageSelectComponent } from "./language-select";
 import { IonicModule } from "@ionic/angular";
-import { HttpClient } from "@angular/common/http";
-import { createTranslateLoader } from "../app.module";
 
 /**
  * Shared and common components used throuhgout the app
